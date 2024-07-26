7039f9e3f67bd4190986ca0c59046a483b70f75c
chlalstj1999 <alstjsms23@gmail.com>
2024-07-26 23:13:51 +0000

    postgre setting add

---
+++ src/common/database/postgre.ts
@@ -0,0 +1,14 @@
+import { Pool } from "pg";
+
+const pool = new Pool({
+  user: "ubuntu",
+  password: "1234",
+  host: "localhost",
+  port: 5432,
+  database: "post",
+  max: 5,
+});
+
+pool.connect();
+
+export default pool;
