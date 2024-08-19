b188452714a4c67e27df11506d5785a33283c8a2
chlalstj1999 <alstjsms23@gmail.com>
2024-08-19 13:29:12 +0900

    change into middleware

--- src/common/pipes/checkAdmin.pipe.ts
+++ src/common/pipes/checkAdmin.pipe.ts
@@ -3,11 +3,13 @@ import { admin } from "../../common/const/role";
 import { UnauthorizedException } from "../../common/exception/UnauthorizedException";
 
 export function checkAdmin() {
-  return function (req: Request, res: Response, next: NextFunction) {
+  return async function (req: Request, res: Response, next: NextFunction) {
     const roleIdx = res.locals.roleIdx;
 
-    if (Number(roleIdx) !== admin) {
+    if (roleIdx !== admin) {
       throw new UnauthorizedException("관리자 권한 필요");
     }
+
+    next();
   };
 }
